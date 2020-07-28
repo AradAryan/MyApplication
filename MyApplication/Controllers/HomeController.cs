31091d08208f16b3a7d0eeb7e8b90062711e92a8
Arad <AradAryanBusiness@gmail.com>
2020-07-28 23:08:23 +0430

    Making Some Changes!

---
+++ MyApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyApplication/infrastructure/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.infrastructure
+{
+    public class BaseController : Controller
+    {
+        public BaseController() : base()
+        {
+        }
+    }
+}
